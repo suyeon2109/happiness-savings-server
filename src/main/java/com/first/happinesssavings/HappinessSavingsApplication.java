c650a7057720223b6b8d4a38b7c5b1e09b21c49b
suyeon <suyeon2109@gmail.com>
2022-05-27 18:35:16 +0900

    initial commit

---
+++ src/main/java/com/first/happinesssavings/HappinessSavingsApplication.java
@@ -0,0 +1,13 @@
+package com.first.happinesssavings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappinessSavingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappinessSavingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/first/happinesssavings/HappinessSavingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.happinesssavings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappinessSavingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
