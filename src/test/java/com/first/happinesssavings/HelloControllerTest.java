ee30cf865cc2a30c13555f39931951b9fa5b95b9
suyeon <suyeon2109@gmail.com>
2022-05-27 19:03:43 +0900

    create Test

---
+++ src/test/java/com/first/happinesssavings/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package com.first.happinesssavings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    void hello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(status().isOk());
+
+    }
+}
